63d70e74da20f016bd19b5d86c7331c361697233
Amr Abdel-Wahab <amr@orcas.io>
2020-07-11 13:07:33 +0200

    Initial Commit

---
+++ NearBy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearBy
+//
+//  Created by Amr AbdelWahab on 7/11/20.
+//  Copyright © 2020 Orcas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
